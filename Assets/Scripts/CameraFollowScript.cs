2feba77f32086960bf7a13e55f6bcbcc1d43a11d
Tymothy <tz300493@student.polsl.pl>
2025-05-07 15:13:55 +0200

    Improved player movement and added a basic Grapple
    Added more functionality to the jump, like holding the spacebar and releasing it to dictate strength of the jump. Also added a basic grappling hook using Line Renderer with a force that pulls the player towards the grapple point and includes gravity.

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 initialOffset;
+
+    void Start()
+    {
+        if (player != null)
+        {
+            initialOffset = transform.position - player.position;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            transform.position = new Vector3(0, player.position.y + initialOffset.y, -10);
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
@@ -8,11 +7,17 @@ public class MovementScript : MonoBehaviour
     private PlayerInput keyPress;
     private InputAction jump;
     private InputAction direction;
-    private float acceleration;
 
     private Rigidbody2D rb2D;
-    private float thrust = 0.25f;
+    private float thrust = 15f;
+    private float jumpHoldTime = 0f;
+    private float maxHoldTime = 1f;
+    private bool isGrounded = true; // Assume grounded initially
+    private bool isJumping = false;
 
+    private float lastHorizontalInput = 0f;
+    private float horizontalInputBufferTime = 0.2f;
+    private float horizontalInputBufferTimer = 0f; 
 
     void Awake()
     {
@@ -33,22 +38,78 @@ public class MovementScript : MonoBehaviour
         direction.Disable();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (jump.ReadValue<float>() > 0)
+        // Update horizontal input buffer
+        float currentHorizontalInput = direction.ReadValue<Vector2>().x;
+        if (Mathf.Abs(currentHorizontalInput) > 0.01f)
         {
-            acceleration = direction.ReadValue<Vector2>().x;
-            Vector2 jumpForce = new Vector2(acceleration, 1) * thrust;
+            lastHorizontalInput = currentHorizontalInput;
+            horizontalInputBufferTimer = horizontalInputBufferTime;
+        }
+        else
+        {
+            horizontalInputBufferTimer -= Time.deltaTime;
+            if (horizontalInputBufferTimer <= 0f)
+            {
+                lastHorizontalInput = 0f;
+            }
+        }
 
-            rb2D.AddForce(jumpForce, ForceMode2D.Impulse);
-            Debug.Log("Jump");
+        if (jump.ReadValue<float>() > 0 && isGrounded)
+        {
+            isJumping = true;
+            jumpHoldTime += Time.deltaTime;
+
+            if (jumpHoldTime >= maxHoldTime)
+            {
+                jumpHoldTime = maxHoldTime;
+                PerformJump();
+                isJumping = false;
+                jumpHoldTime = 0f;
+            }
+        }
+        else if (isJumping)
+        {
+            PerformJump();
+            isJumping = false;
+            jumpHoldTime = 0f;
         }
     }
+
+    private void PerformJump()
+    {
+        if (isGrounded)
+        {
+            float jumpStrength = (jumpHoldTime / maxHoldTime) * thrust;
+
+            Vector2 jumpForce = new Vector2(lastHorizontalInput, 1).normalized * jumpStrength;
+
+            rb2D.AddForce(jumpForce, ForceMode2D.Impulse);
+            Debug.Log($"Jump with strength: {jumpStrength}, direction: {lastHorizontalInput}");
+
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+
+            // Reset grappling hook usage when grounded
+            GrapplingHookScript grapplingHook = GetComponent<GrapplingHookScript>();
+            if (grapplingHook != null)
+            {
+                grapplingHook.ResetGrappleUsage();
+            }
+        }
+    }
+
 }
